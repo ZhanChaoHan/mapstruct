242eb7fca1a068d1ded7eea4eac0b010e9237ce5
jachs <799516423@qq.com>
2021-06-28 17:32:31 +0800

    'qec'

---
+++ mapstruct/src/main/java/com/jachs/mapstruct/domain/User.java
@@ -0,0 +1,15 @@
+package com.jachs.mapstruct.domain;
+
+/***
+ * 
+ * @author zhanchaohan
+ *
+ */
+public class User {
+	private String userName;
+	private int userAge;
+	private boolean isBoy;
+	private String email;
+	private long phoneNum;
+	
+}
